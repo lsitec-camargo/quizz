0b4e8722ca908b31e62b1852ddfb0c7c405b6574
lsitec-camargo <andressa.camargo@lsitec.org.br>
2022-05-31 10:40:54 -0300

    Data binding and GameViewModel
    Refactor, added data binding and game view model.
    Changed the Question model to generate a random list of answers (correct and incorrect)

--- app/src/main/java/br/org/lsitec/android/quizz/model/Question.kt
+++ app/src/main/java/br/org/lsitec/android/quizz/model/Question.kt
@@ -2,15 +2,20 @@ package br.org.lsitec.android.quizz.model
 
 class Question(
     val id: String,
-    val category: String,
     val question: String,
     val correctAnswer: String,
     val incorrectAnswers: List<String>,
-    val tags: List<String>,
-    val type: String,
-    val difficulty: String
 ) {
+
     override fun toString(): String {
-        return "$question correctAnswer: $correctAnswer"
+        return "Question(question='$question', correctAnswer='$correctAnswer', incorrectAnswers=$incorrectAnswers)"
     }
+
+    fun shuffleAnswers(): List<String> {
+        val answers: MutableList<String> = incorrectAnswers.toMutableList()
+        answers.add(correctAnswer)
+        answers.shuffle()
+        return answers
+    }
+
 }
