175ac4f41f407f34ff11bbb2d874ce12437a5ec1
lsitec-camargo <andressa.camargo@lsitec.org.br>
2022-06-01 19:35:06 -0300

    Loading view and Error view
    Added GameStatus to control which view is going to appear.
    - Loading -> Loading view
    - Done - It shows the game
    - Else - An error occurred

--- app/src/main/java/br/org/lsitec/android/quizz/webclient/QuizWebClient.kt
+++ app/src/main/java/br/org/lsitec/android/quizz/webclient/QuizWebClient.kt
@@ -1,24 +1,14 @@
 package br.org.lsitec.android.quizz.webclient
 
-import android.util.Log
 import br.org.lsitec.android.quizz.model.Question
 import br.org.lsitec.android.quizz.webclient.services.QuizService
-import java.lang.Exception
-
-private const val TAG = "QuizWebClient"
+import retrofit2.Response
 
 class QuizWebClient {
 
     private val quizService: QuizService = RetrofitInstance().quizService
 
-    suspend fun getRandomQuestions(): List<Question>? {
-        val apiResponse = quizService.getRandomQuestions()
-        return try {
-            Log.i(TAG, "response code: ${apiResponse.code()}")
-            apiResponse.body()
-        } catch (e: Exception) {
-            Log.i(TAG, "response code: ${apiResponse.code()}, error: $e")
-            null
-        }
+    suspend fun getRandomQuestions(): Response<List<Question>> {
+        return quizService.getRandomQuestions()
     }
 }
